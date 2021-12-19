a07e9a72eff8b2cd14fe9ae67a628e93cfe686d3
Dakota Madden-Fong <maddenfong@gmail.com>
2021-12-19 11:51:59 -0800

    more interface work

--- src/components/Chaining.tsx
+++ src/components/Chaining.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import {
     Box,
-    Typography,
     Select,
     MenuItem,
+    FormControl,
+    FormHelperText,
+    InputLabel,
     makeStyles,
 } from '@material-ui/core';
 
@@ -36,11 +38,76 @@ const Chaining = (props: {}): JSX.Element => {
         map((poke) => poke.name).
         map((name) => <MenuItem key={name} value={name}>{name}</MenuItem>);
 
+    const startingPokemon = useAppSelector(selectStartingPokemon);
+    const endingPokemon = useAppSelector(selectEndingPokemon);
+
+    // handler functions
+    const handleStarting = (event: React.ChangeEvent<{
+        name?: string | undefined;
+        value: unknown;
+    }>) => {
+
+        if (typeof event.target.value === 'string') {
+
+            dispatch(setStarting(event.target.value));
+
+        }
+
+    };
+
+    const handleEnding = (event: React.ChangeEvent<{
+        name?: string | undefined;
+        value: unknown;
+    }>) => {
+
+        if (typeof event.target.value === 'string') {
+
+            dispatch(setEnding(event.target.value));
+
+        }
+
+    };
+
     return (
         <Box className={classes.root}>
-            <Select>
-                {pokemon}
-            </Select>
+
+            {/* Starting Pokemon Input */}
+            <FormControl>
+                <InputLabel
+                    shrink
+                    id='starting-pokemon-input-label'
+                >
+                    Starting Pokemon
+                </InputLabel>
+                <Select
+                    value={startingPokemon}
+                    onChange={handleStarting}
+                    variant='standard'
+                    labelId='starting-pokemon-input-label'
+                >
+                    {pokemon}
+                </Select>
+                <FormHelperText>Starting Pokemon</FormHelperText>
+            </FormControl>
+
+            {/* Ending Pokemon Input */}
+            <FormControl>
+                <InputLabel
+                    shrink
+                    id='ending-pokemon-input-label'
+                >
+                    Ending Pokemon
+                </InputLabel>
+                <Select
+                    value={endingPokemon}
+                    onChange={handleEnding}
+                    variant='standard'
+                    labelId='ending-pokemon-input-label'
+                >
+                    {pokemon}
+                </Select>
+                <FormHelperText>Ending Pokemon</FormHelperText>
+            </FormControl>
         </Box>
     );
 
